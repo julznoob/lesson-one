205e24432145484cb85c3d439fba989dbfc81d0f
julznoob <--global>
2023-04-06 12:45:51 +1000

    content headers are important!

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 3000;
 const htmlServer = http.createServer((req, res) => {
 
     const reqUrl = new URL(req.url, `http://${hostname}:${port}`)
+    const isCSS = /\.css$/.test(req.url)
 
     // if we don't have an extension, add .html, otherwise, add nothing.
     const staticFilePath = path.normalize(path.format({
@@ -57,7 +58,7 @@ const htmlServer = http.createServer((req, res) => {
             res.end(`<h1>Nothing to see here</h1>`)
         } else {
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html');
+            res.setHeader('Content-Type', isCSS ? 'text/css' : 'text/html');
             fs.createReadStream(staticFilePath).pipe(res)
         }
     })
